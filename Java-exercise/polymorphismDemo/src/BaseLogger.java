129f4e3d029ec4cd9b6332ccc55c1a9d83c006fa
Dursun <93613110+Dursunipek@users.noreply.github.com>
2022-07-15 19:31:12 +0300

    Java Polymorphism

---
+++ Java-exercise/polymorphismDemo/src/BaseLogger.java
@@ -0,0 +1,5 @@
+public class BaseLogger {
+    public void log(String message){
+        // BaseLogger class'ini miras alan diger class'lar override yaparak bu metod farkli sekillerde kullanilabilir
+    }
+}
---
+++ Java-exercise/polymorphismDemo/src/Main.java
@@ -0,0 +1,26 @@
+public class Main {
+    public static void main(String[] args) {
+        // Polymorphism: Cok bicimlilik
+        // Overwriting ve overloading gibi metodlar kullanabiliriz
+        // Overwriting: Bir class'in baska bir class'dan miras aldigi metodu degistirerek tamamen yeni bir metod yazma islemi
+        // DatabaseLogger logger = new DatabaseLogger();
+        // logger.Log("Log message");
+
+        // Bu islemi cok bicimli bir sekilde yapmak icin ayni metodun for dongusu ile farkli class'larda kullanildigi bir ornek;
+        BaseLogger[] loggers = new BaseLogger[]{new FileLogger(),new EmailLogger(),new DatabaseLogger()};
+        for(BaseLogger logger : loggers){
+            logger.log("Log message");
+        }
+        // Bu sekilde polymorphism sayesinde sistem kolaylikla güncellenebilir ve gelistirilebilir oluyor
+        // Herhangi bir ekleme yapilmasi gerekirse inheritance yapilarak farkli class'lar ayni metodu kullanabilir.
+
+        CustomerManager customerManager = new CustomerManager(new DatabaseLogger());
+        customerManager.addCustomer();
+        CustomerManager customerManager1 = new CustomerManager(new FileLogger());
+        customerManager1.addCustomer();
+        // Loglama orneginde olusturdugumuz CustomerManager class'ı gelecekte farkli sekillerde loglanabilecegi icin loglamayi sabit metod icinde yapmadik
+        // Loglama isleminin yapilacagi yeri constructor ile kullanicidan alarak metod icine aktardik
+        // Cok bicimlilik ile de farkli log alanlarina erisebilmek icin BaseLogger class'ini kullandik
+        // Diger class'lar BaseLogger'a referans olabildigi icin constructor'da belirtilen logger'a gore cikti almamizi sagladi
+    }
+}
